4e7dc66779a8dc56210bd6f93b2912619843bdc2
bagusshndr31 <bagus.suhendriyatno@salt.co.id>
2023-01-04 00:54:08 +0700

    S

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.19
 
 require (
 	github.com/gorilla/websocket v1.5.0
-	github.com/novalagung/gubrak/v2 v2.0.1
+
 )
--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"chat-golang-api/pkg"
+	"flag"
+	"log"
 	"net/http"
-
-	"github.com/bagusshndr/chat-golang-api/pkg/websocket"
 )
 
-func setupRoutes() {
-	pool := websocket.NewPool()
-	go pool.Start()
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
 }
 
 func main() {
-	setupRoutes()
-
-	http.ListenAndServe(":9090", nil)
+	flag.Parse()
+	hub := pkg.NewHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		pkg.serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
